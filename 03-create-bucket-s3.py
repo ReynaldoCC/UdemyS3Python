7e36d6e948dec9ccddcdc50602718cac91503709
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2023-06-01 15:17:58 +0700

    class 3 creating buckets

---
+++ 03-create-bucket-s3.py
@@ -0,0 +1,13 @@
+import boto3
+
+import utils
+
+NEW_BUCKET_NAME = 'testing-bucket-sss-new'
+
+client = boto3.client('s3')
+
+utils.show_bucket_list(client=client)
+
+client.create_bucket(Bucket=NEW_BUCKET_NAME)
+
+utils.show_bucket_list(client=client)
